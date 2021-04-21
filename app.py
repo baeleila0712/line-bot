6e2316fb69739c74f8f569de66ba2eaf315280fe
BAe <bae0712bar@gmail.com>
2021-04-21 21:43:09 +0800

    change5

--- app.py
+++ app.py
@@ -39,21 +39,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = sticker_message
     r = '請輸入早安/你好/愛你/想你/情侶間常用的關鍵字哦'
 
-        if msg == '早安':
+    if msg == '早安':
         r = '早安阿寶～祝你有美好的一天哦'
 
     elif msg == '餓了':
         r = '我也餓了，你吃飯了嗎'
-        
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
-
-
-
 
 
 if __name__ == "__main__":
